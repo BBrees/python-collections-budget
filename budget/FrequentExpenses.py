2472da08f500f1364ecc834fe0689614ea2937d1
Beckett Brees <bbreespt@gmail.com>
2021-11-09 08:35:48 -0500

    developed Frequent Expenses file

---
+++ budget/FrequentExpenses.py
@@ -0,0 +1,27 @@
+from . import Expense
+import collections
+
+import matplotlib.pyplot as plt
+
+expenses = Expense.Expenses()
+
+Expense.read_expenses(expenses, data/spending_data.csv)
+
+spending_categories = []
+
+for expense in expenses.list:
+    append.expense_category(spending_categories)
+
+spending_counter = collections.Counter(spending_categories)
+
+top5 = spending_counter.most_common(5)
+
+categories, count = zip(*top5)
+
+fig, ax = plt.subplot()
+
+ax.bar(categories, count)
+
+ax.set_title('# of Purchases by Category')
+
+plt.show()
